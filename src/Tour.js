4079bd23d63377e916bea68aad4a0bdfd3423f50
rahul dubey <rahul.dubey@dianapps.com>
2021-12-21 11:08:42 +0530

    commit

--- src/Tour.js
+++ src/Tour.js
@@ -1,19 +1,32 @@
 import react, { useState }from "react";
 
-const Tour = ({id,image,info,price,name,removeTour}) => {
+const Tour = ({ id,image,info,price,name,removeTour,isSubmit,addTours,tours}) => {
+    console.log(tours.length)
     const [readMore, setReadMore] = useState(false);
     return <article className="single-tour">
         <img src={image} alt={name} />
         <footer>
-            <div className="tour-info">
-                <h4>{name}</h4>
-                <h4 className="tour-price">${price}</h4>
-            </div>
-            <p>{readMore ? info : `${info.substring(0,200)}...`}</p>
-            <button onClick={()=>setReadMore(!readMore)}> 
-                {readMore?'Show less':'Read more'}
-            </button>
-            <button className="delete-btn" onClick={removeTour(id)}>Not interested</button>
+            {tours.length>0 ? 
+                <div>
+                    <div className="tour-info">
+                        <h4>{name}</h4>
+                        <h4 className="tour-price">${price}</h4>
+                    </div>
+                    <p>{readMore ? info : `${info.substring(0,200)}...`}</p>
+                    <button onClick={()=>setReadMore(!readMore)}> 
+                        {readMore?'Show less':'Read more'}
+                    </button>
+                    {isSubmit ? 
+                    <button className="delete-btn" onClick={()=>addTours(id)}>Interested</button>
+                    :
+                    <button className="delete-btn" onClick={()=>removeTour(id)}>Not interested</button>
+                    }
+                </div>
+                :
+                <div>
+                    <h1>No Tours found</h1>
+                </div>
+            }
         </footer>
     </article>
 };
--- src/Tours.js
+++ src/Tours.js
@@ -1,18 +1,57 @@
-import react from 'react';
+import react, {useState, useEffect} from 'react';
 import Tour from './Tour'
 
-const Tours = (tours,removeTour) =>{
-    console.log(tours)
+const Tours = ({ tours,removeTour,unlikedTours,addTours }) =>{    
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [notLiked, setNotLiked] = useState();
+    
+    useEffect(() =>{
+      setNotLiked(unlikedTours);
+      });
+
+    const moveAll = () => {
+        console.log(tours)
+        setNotLiked(tours);
+        setIsSubmit(true);
+        console.log(notLiked);
+    }
     return (
         <section>
-            <div className='title'>
-                <h2>Ours Tours</h2>
-                <div className='underline'></div>  
-            </div>
             <div>
-                {tours.tours?.map((tour) => {
-                    return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
-                })}  
+                <div className='title'>
+                    <h2 className={isSubmit? '' : 'active'}onClick={()=>setIsSubmit(false)}>Ours Tours</h2>
+                    <h2 className={isSubmit? 'active' : ''}onClick={()=>setIsSubmit(true)}>Show not interested</h2>
+                    <h2 onClick={moveAll}>Move all to not interested</h2>
+                </div>
+                {isSubmit ? 
+                <div>111
+                    {notLiked.length>0 ?
+                        <div>
+                            {notLiked.map((tour) => {
+                                return <Tour key={tour.id} {...tour} isSubmit={isSubmit} removeTour={removeTour} addTours={addTours} tours={notLiked}></Tour>;
+                            })}  
+                        </div>
+                        :
+                        <div className='no-tour'>
+                            <h1>No Tours Found</h1>
+                        </div>
+                    }
+                </div>
+                :
+                <div>222
+                    {tours.length>0 ? 
+                        <div>
+                            {tours.map((tour) => {
+                                return <Tour key={tour.id} {...tour} isSubmit={isSubmit} removeTour={removeTour} addTours={addTours} tours={tours}></Tour>;
+                            })}
+                        </div>
+                        :
+                        <div className='no-tour'>
+                            <h1>No Tours Found</h1>
+                        </div>
+                    }  
+                </div> 
+                }
             </div>
         </section>
     );
